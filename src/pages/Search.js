6c4244b8378599c382f5a23213c758501691f7cd
Daniel Kinyunyu <152310839+dani-2001@users.noreply.github.com>
2026-03-24 17:36:00 +0300

    Create Search.js file with search results page component

---
+++ src/pages/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div>
+      <h1>Search Results</h1>
+      <p>No results found.</p>
+    </div>
+  );
+};
+
+export default Search;
