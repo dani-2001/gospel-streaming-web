29a87e5e7d150511d2c7b725924e72c4db420b7a
Daniel Kinyunyu <152310839+dani-2001@users.noreply.github.com>
2026-03-24 17:37:25 +0300

    Create Home.js component for video feed and recommendations.

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Video Feed</h1>
+      {/* Video feed component will go here */}
+      <div>
+        {/* Place video feed logic here */}
+      </div>
+      <h2>Recommendations</h2>
+      {/* Recommendations component will go here */}
+      <div>
+        {/* Place recommendations logic here */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
