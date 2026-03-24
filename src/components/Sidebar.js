dd9968b85a63cc5c943d71aa371763171cefd094
Daniel Kinyunyu <152310839+dani-2001@users.noreply.github.com>
2026-03-24 17:35:16 +0300

    Create Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h2>Navigation</h2>
+            <ul>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/services">Services</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
